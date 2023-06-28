34bb4fa20a832c1b17d694b7bfc531a01952dc7d
arnablegend <arnab.cs13@gmail.com>
2023-06-28 22:56:33 +0530

    Update MainActivity
    This commit
    
    1. Adds helper function to copy assets
    2. Adds function to run application in native lib directory
    3. Adds execution comments
    4. Adds better logs
    
    Signed-off-by: arnablegend <arnab.cs13@gmail.com>

--- app/src/main/java/com/example/binarywrapper/MainActivity.java
+++ app/src/main/java/com/example/binarywrapper/MainActivity.java
@@ -3,21 +3,21 @@ package com.example.binarywrapper;
 import com.example.binarywrapper.DumpFile;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.file.StandardCopyOption;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
 
-import android.content.Context;
-import java.io.FileOutputStream;
-import java.io.IOException;
 
 
 
@@ -30,24 +30,68 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String command = "ls -l /sdcard";
+        // Create Log File
         Date d = new Date();
-
-        File dir = getApplicationContext().getFilesDir();
-        Log.d(LOG_TAG, "Start writing");
         DumpFile dump = new DumpFile();
         dump.writeToFile(getApplicationContext(), LOG_FILE, d.toString() + "\n");
+
+        // Check lib folder. This is the path where packaged .so files
+        // will be dumped into the device. The same path where executables
+        // will also be kept as .so files
+        String command = "ls -l " + getApplicationInfo().nativeLibraryDir;
         dump.appendToFile(getApplicationContext(), LOG_FILE, command + "\n");
         dump.appendToFile(getApplicationContext(), LOG_FILE, runCommand(command) + "\n");
+
+        // Execute binary. This app will not run out of the box. Values need
+        // to be updated with exact values.
+        // #CHANGEME
+        List<String> commands = Arrays.asList("./binary.so"
+            ,"arg1"
+            ,"arg2");
+        String exec_command = Arrays.toString(commands.toArray());
+        dump.appendToFile(getApplicationContext(), LOG_FILE, exec_command + "\n");
+        dump.appendToFile(getApplicationContext(), LOG_FILE, command + "\n");
         Log.d(LOG_TAG, "Read data : " + dump.readFromFile(getApplicationContext(), LOG_FILE));
     }
 
-    public String runCommand(String command) {
+    private String runCommand(List<String> commands){
+        StringBuilder output = new StringBuilder();
+        try{
+            ProcessBuilder builder = new ProcessBuilder();
+            // Change to execute directory
+            builder.directory(new File(getApplicationInfo().nativeLibraryDir));
+            Map<String, String> env_kv = builder.environment();
+            env_kv.put("LD_LIBRARY_PATH", "/system/vendor/lib64"); // #CHANGEME
+            builder.command(commands);
+            builder.redirectErrorStream(true);
+
+            Log.d(LOG_TAG, "Starting process");
+            Process process = builder.start();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+            String line;
+            Log.d(LOG_TAG, "Capturing output");
+            while((line = reader.readLine()) != null){
+                output.append(line + "\n");
+            }
+            reader.close();
+            process.waitFor();
+            Log.d(LOG_TAG, "Process finished");
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "IO Exception", e);
+        } catch (InterruptedException e) {
+            Log.e(LOG_TAG, "Interrupted Exception", e);
+        }
+        return output.toString();
+    }
+    private String runCommand(String command) {
         StringBuilder output = new StringBuilder();
         try {
+            Log.d(LOG_TAG, "Exec process");
             Process process = Runtime.getRuntime().exec(command);
             BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
 
+            Log.d(LOG_TAG, "Capturing output");
             String line;
             while ((line = reader.readLine()) != null) {
                 output.append(line);
@@ -55,11 +99,28 @@ public class MainActivity extends AppCompatActivity {
             }
 
             reader.close();
+            process.waitFor();
+            Log.d(LOG_TAG, "Process finished");
         } catch (IOException e) {
-            e.printStackTrace();
+            Log.e(LOG_TAG, "IO Exception", e);
+        } catch (InterruptedException e) {
+            Log.e(LOG_TAG, "Interrupted Exception", e);
         }
 
         return output.toString();
     }
 
+    // Helper function to copy Asset files
+    private void copyAsset(String filename){
+        try {
+            Log.d(LOG_TAG, "Copying asset : " + filename);
+            InputStream in_file = getAssets().open(filename);
+            File targetFile = new File(getApplicationContext().getFilesDir().getPath() + "/" + filename);
+
+            java.nio.file.Files.copy(in_file, targetFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+            in_file.close();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "IO Exception", e);
+        }
+    }
 }
